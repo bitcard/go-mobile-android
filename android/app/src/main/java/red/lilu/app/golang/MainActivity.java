f4bfe37ef2f186a9ea0b19df588d8588a2c2aaa8
李小明 <likeming@newpipeline.cn>
2019-04-28 15:29:51 +0800

    添加GoLang生成文件示例.

--- android/app/src/main/java/red/lilu/app/golang/MainActivity.java
+++ android/app/src/main/java/red/lilu/app/golang/MainActivity.java
@@ -1,13 +1,36 @@
 package red.lilu.app.golang;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.PermissionChecker;
 
 import hi.Hi;
 
 public class MainActivity extends AppCompatActivity {
+
+  private static final String T = MainActivity.class.getName();
+
+  private void file() {
+    Hi.hiFile("小红");
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    if (requestCode == 1 && grantResults.length == 0) {
+      file();
+    } else {
+      Toast.makeText(this, "没有允许权限!", Toast.LENGTH_LONG)
+          .show();
+    }
+  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +39,18 @@ public class MainActivity extends AppCompatActivity {
 
     TextView text = findViewById(R.id.text);
     text.setText(
-        Hi.hi("小明")
+        Hi.hiText("小明")
     );
+
+    if (PermissionChecker.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PermissionChecker.PERMISSION_DENIED) {
+      Log.d(T, "申请权限");
+      requestPermissions(
+          new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+          1
+      );
+    } else {
+      Log.d(T, "已有权限");
+      file();
+    }
   }
 }
--- golang/hi/hi.go
+++ golang/hi/hi.go
@@ -2,11 +2,25 @@ package hi
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"strings"
 )
 
-func Hi(name string) string {
+//简单方法
+func HiText(name string) string {
 	fmt.Println("你好", name)
 
 	return strings.Join([]string{"你好", name}, ":")
+}
+
+//在外部存储中生成"你好.txt"文件
+func HiFile(name string) {
+	log.Println("生成文件")
+
+	bytes := []byte(HiText(name))
+	err := ioutil.WriteFile("/sdcard/你好.txt", bytes, 0644)
+	if err != nil {
+		log.Println(err)
+	}
 }
